0752ea62185299a4ab76ecc7e552befe8aecf5df
gajendrahedau <43582958+gajendrahedau@users.noreply.github.com>
2020-03-29 15:58:55 +0530

    Update TestController.java

--- src/main/java/com/javainuse/springboothelloworld/controller/TestController.java
+++ src/main/java/com/javainuse/springboothelloworld/controller/TestController.java
@@ -7,9 +7,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TestController {
 
+	//@RequestMapping("/welcome")
+	//public ModelAndView firstPage() {
+	//	return new ModelAndView("welcome");
+	//}
+	
 	@RequestMapping("/welcome")
-	public ModelAndView firstPage() {
-		return new ModelAndView("welcome");
+	public String firstPage() {
+		return "welcome to Spring boot";
 	}
 
 }
